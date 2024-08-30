b2ef812abeb092cf981168e4d7261a4207c75713
Anna <anna.dybinina.1@gmail.com>
2024-08-30 15:48:25 +0300

    created FormContainer component

---
+++ components/form/FormContainer.tsx
@@ -0,0 +1,30 @@
+"use client";
+
+import { useFormState } from "react-dom";
+import { useEffect } from "react";
+import { useToast } from "@/components/ui/use-toast";
+import { ActionFunction } from "@/utils/types";
+
+const initialState = {
+  message: "",
+};
+
+function FormContainer({
+  action,
+  children,
+}: {
+  action: ActionFunction;
+  children: React.ReactNode;
+}) {
+  const [state, formAction] = useFormState(action, initialState);
+  const { toast } = useToast();
+
+  useEffect(() => {
+    if (state.message) {
+      toast({ description: state.message });
+    }
+  }, [state]);
+
+  return <form action={formAction}>{children}</form>;
+}
+export default FormContainer;
