f39f3346d13a462cd67a11356252a051eba05f25
Anna <anna.dybinina.1@gmail.com>
2024-08-30 19:19:21 +0300

    created fetchProfile utility fn & updateProfileAction mock

--- utils/actions.ts
+++ utils/actions.ts
@@ -6,6 +6,17 @@ import { auth, clerkClient, currentUser } from "@clerk/nextjs/server";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 
+const getAuthUser = async () => {
+  const user = await currentUser();
+  if (!user) {
+    throw new Error("You must be logedin to access this route");
+  }
+  if (!user?.privateMetadata?.hasProfile) {
+    redirect("/profile/create");
+  }
+  return user;
+};
+
 export const createProfileAction = async (
   prevState: any,
   formData: FormData
@@ -42,6 +53,7 @@ export const createProfileAction = async (
 
 export const fetchProfileImage = async () => {
   const user = await currentUser();
+
   if (!user) {
     return null;
   }
@@ -52,4 +64,23 @@ export const fetchProfileImage = async () => {
   });
 
   return profile?.profileImage;
+};
+
+export const fetchProfile = async () => {
+  const user = await getAuthUser();
+
+  const profile = await db.profile.findUnique({
+    where: { clerkId: user.id },
+  });
+
+  if (!profile) redirect("/profile/create");
+
+  return profile;
+};
+
+export const updateProfileAction = async (
+  prevState: any,
+  formData: FormData
+): Promise<{ message: string }> => {
+  return { message: "update profile action" };
 };
