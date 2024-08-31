6baa128307b5801329c1c7e1fd59bfeb15d1297d
Anna <anna.dybinina.1@gmail.com>
2024-08-31 17:50:06 +0300

    added image upload component to user profile page

---
+++ components/form/ImageInputContainer.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import { useState } from "react";
+import Image from "next/image";
+import { Button } from "../ui/button";
+import FormContainer from "./FormContainer";
+import ImageInput from "./ImageInput";
+import { SubmitButton } from "./Buttons";
+import { type ActionFunction } from "@/utils/types";
+import { LuUser2 } from "react-icons/lu";
+
+type ImageInputContainerProps = {
+  image: string;
+  name: string;
+  action: ActionFunction;
+  text: string;
+  children?: React.ReactNode;
+};
+
+function ImageInputContainer({
+  image,
+  name,
+  action,
+  text,
+  children,
+}: ImageInputContainerProps) {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const userIcon = (
+    <LuUser2 className="w-24 h-24 bg-primary rounded text-white mb-4" />
+  );
+
+  return (
+    <div>
+      {image ? (
+        <Image
+          src={image}
+          alt={name}
+          width={100}
+          height={100}
+          className="rounded object-cover mb-4 w-24 h-24"
+        />
+      ) : (
+        userIcon
+      )}
+      <Button
+        variant="outline"
+        size="sm"
+        onClick={() => setIsOpen((prev) => !prev)}
+      >
+        {text}
+      </Button>
+      {isOpen && (
+        <div className="max-w-lg mt-4">
+          <FormContainer action={action}>
+            {children}
+            <ImageInput />
+            <SubmitButton size="sm" />
+          </FormContainer>
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default ImageInputContainer;
